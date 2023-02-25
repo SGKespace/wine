915e36b56316acd2e851c412e11f52ae6ebba649
SGK <55636018+SGKespace@users.noreply.github.com>
2023-02-25 13:09:46 +0400

    Исправления после ревью

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 import datetime
 import additional_functions as add_f
+from dotenv import load_dotenv
 
 
 def main():
-    year_of_creation = 1920
-    years_since_inception = datetime.datetime.now().year - year_of_creation
+    dotenv.load_dotenv()
+    excel_path = os.getenv("EXCEL_PATH")
+    start_year = os.getenv("START_YEAR")
+
+    years_since_inception = datetime.datetime.now().year - start_year
     form_year = add_f.get_form_word_year(years_since_inception)
-    wines = add_f.get_exel_wine('wine.xlsx')
+    wines = add_f.get_exel_wine(excel_path)
     drink_groups = add_f.get_drink_groups(wines)
     add_f.change_index_html(years_since_inception, form_year, drink_groups)
 
     server = HTTPServer(('127.0.0.1', 8000), SimpleHTTPRequestHandler)
     server.serve_forever()
+
 
 if __name__ == "__main__":
     main()
